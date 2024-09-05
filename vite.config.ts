16dc3e7c1b08e62eae9c9915d60dbb270949d90e
ImanolBraccciale <imanol11699@outlook.com>
2024-09-04 22:52:02 -0300

    asd

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"https://tuspiesenmismanos.grupoib.website",
+  base:"/",
   plugins: [react()],
 })
